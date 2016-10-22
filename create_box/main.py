b138a60b23aaadcc461a03036defe0493442e784
Joel Musante <jmnyc88@yahoo.com>
2016-10-22 16:34:54 +0000

    Box works

--- create_box/main.py
+++ create_box/main.py
@@ -1,9 +1,39 @@
 """This is the entry point of the program."""
-
-
 def create_box(height, width, character):
-    pass
+    box = ''
+    for i in range(height):
+        for j in range(width):
+            box += character
+        box += '\n'
+    return box
+
+def create_box_empty(height, width, character):
+	s = ""
+
+	height = int(height)
+	width = int(width)
+		
+	for i in range(height):
+		#print("*", end="")
+		for j in range(width):
+			if j == 0 or j == width-1 or i == 0 or i == height-1:
+				s += "*" + ""
+			else:
+				s += " " + ''
+		s += "\n"
+
+	return(s)
+                
+       
+            
+    
+    
+
 
 
 if __name__ == '__main__':
-    create_box(3, 4, '*')
+    
+        
+    print(create_box(3,4, '*'))
+
+
